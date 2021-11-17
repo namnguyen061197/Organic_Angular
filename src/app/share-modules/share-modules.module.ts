a006502d8ab6f345ee925b7532d147aaa638f634
namnguyen061197 <namnguyen20152584@gmail.com>
2021-11-17 17:00:41 +0700

    [nam] - create repo

---
+++ src/app/share-modules/share-modules.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PaginationComponent } from './pagination/pagination.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    PaginationComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ShareModulesModule { }
